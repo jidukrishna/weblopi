2d01491b021bc4131f293d5f8d6dd5c412a7246b
Jidu Krishna P J <74850929+jidukrishna@users.noreply.github.com>
2024-09-21 02:24:24 +0530

    changed to python3

--- weblopi.sh
+++ weblopi.sh
@@ -4,5 +4,5 @@ source path/to/pythonenv/bin/activate
 
 sleep 5
 cd /path/to/directory/stored
-nohup python main.py > /dev/null 2>&1 &
-nohup python api_web.py > /dev/null 2>&1 &
+nohup python3 main.py > /dev/null 2>&1 &
+nohup python3 api_web.py > /dev/null 2>&1 &
